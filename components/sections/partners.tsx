1e4dffb4a6819690bc4972dbe80bbf089bd21872
ardasaracgm <ardasaracgm@gmail.com>
2026-03-30 17:59:48 +0300

    Update partners.tsx

--- components/sections/partners.tsx
+++ components/sections/partners.tsx
@@ -1,1 +1,24 @@
+import Container from "@/components/ui/container";
 
+const partners = [
+  "Savunma Teknolojileri",
+  "Lojistik ve Tedarik",
+  "Proje Çözümleri",
+  "Teknik Eğitim",
+];
+
+export default function PartnersSection() {
+  return (
+    <section className="section-tight">
+      <Container>
+        <div className="grid-4">
+          {partners.map((partner) => (
+            <div key={partner} className="partner-card">
+              {partner}
+            </div>
+          ))}
+        </div>
+      </Container>
+    </section>
+  );
+}
