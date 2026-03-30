3e3266c6c3d88f8e20dbd4bf619d6f76f723d8f4
ardasaracgm <ardasaracgm@gmail.com>
2026-03-30 17:58:56 +0300

    Update highlights.tsx

--- components/sections/highlights.tsx
+++ components/sections/highlights.tsx
@@ -1,1 +1,24 @@
+import Container from "@/components/ui/container";
 
+const highlights = [
+  "Danışmanlık ve proje geliştirme odaklı kurumsal yapı",
+  "Lojistik, temin ve tedarik süreçlerine hâkim yaklaşım",
+  "Teknik değerlendirme ve uygulama desteği",
+  "Savunma ve havacılık alanına uygun modern sunum dili",
+];
+
+export default function HighlightsSection() {
+  return (
+    <section className="section">
+      <Container>
+        <div className="grid-4">
+          {highlights.map((item) => (
+            <div key={item} className="info-card">
+              {item}
+            </div>
+          ))}
+        </div>
+      </Container>
+    </section>
+  );
+}
