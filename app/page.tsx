fc768b2d08ceb66059022788370bddc3a292e44b
ardasaracgm <ardasaracgm@gmail.com>
2026-03-30 17:53:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,23 @@
+import Header from "@/components/layout/header";
+import HeroSlider from "@/components/sections/hero-slider";
+import ServicesSection from "@/components/sections/services";
+import ApproachSection from "@/components/sections/approach";
+import HighlightsSection from "@/components/sections/highlights";
+import PartnersSection from "@/components/sections/partners";
+import QualitySection from "@/components/sections/quality";
+import ContactSection from "@/components/sections/contact";
 
+export default function HomePage() {
+  return (
+    <main>
+      <Header />
+      <HeroSlider />
+      <ServicesSection />
+      <ApproachSection />
+      <HighlightsSection />
+      <PartnersSection />
+      <QualitySection />
+      <ContactSection />
+    </main>
+  );
+}
